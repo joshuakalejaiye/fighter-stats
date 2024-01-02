b59cd66586ba33b853830020ca1af775281f8b62
Joshua Kalejaiye <joshua.kalejaiye@sainsburys.co.uk>
2024-01-02 18:13:52 +0000

    feat: disable caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { getHomepageGames } from '@/server/steam-actions'
 import { GamesTable } from '@/components/games-table'
 import Banner from '@/components/banner'
 
+export const dynamic = 'force-dynamic'
+
 export default async function HomePage() {
   const [firstGameId, ...remainingGameIds]  = await getHomepageGames()
 

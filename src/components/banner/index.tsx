94f649e67b3cdbbe3fb05d7de7a55077c45bee27
Joshua Kalejaiye <joshua.kalejaiye@sainsburys.co.uk>
2024-01-03 15:54:24 +0000

    test: use client components

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,24 +1,21 @@
 "use client"
 
+import type { Game } from "@/index"
 import { useEffect, useState } from "react"
 
 export default function Banner({ steamId }: { steamId: number }) {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<Game>()
   const [isLoading, setLoading] = useState(true)
  
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     fetch(`/api/games/${steamId}`)
       .then((res) => res.json())
-      .then((data) => {
+      .then((data: { data: Game}) => {
         setData(data.data)
         setLoading(false)
       })
   }, [])
-
-  useEffect(() => {
-    console.log(data)
-  }, [data])
  
   if (isLoading) return <p>Loading...</p>
   if (!data) {

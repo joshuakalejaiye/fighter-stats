898096fc2b6d5ce0b0f7595ef4236961d90d5ad8
Joshua Kalejaiye <joshua.kalejaiye@sainsburys.co.uk>
2024-01-08 19:32:10 +0000

    feat: spotlight on the rise

--- src/components/card-grid/index.tsx
+++ src/components/card-grid/index.tsx
@@ -1,20 +1,42 @@
 import { GameCard } from '../game-card'
 import { prisma } from '@/server/db'
-import { TEKKEN_8 } from '@/index.enums'
 
 export default async function CardGrid() {
-  const [one, two] = await prisma.games.findMany({
+  const [mostPopular] = await prisma.games.findMany({
     orderBy: {
       players: 'desc',
     },
     select: {
       steam_id: true,
     },
-    take: 2,
+    take: 1,
   })
 
-  const firstGameId = Number(one?.steam_id)
-  const secondGameId = Number(two?.steam_id)
+  const onTheRise = await prisma.games.findFirst({
+    where: {
+      accolades: {
+        accolade: 'on the rise',
+      },
+    },
+    select: {
+      steam_id: true,
+    },
+  })
+
+  const mostAnticipated = await prisma.games.findFirst({
+    where: {
+      accolades: {
+        accolade: 'most anticipated',
+      },
+    },
+    select: {
+      steam_id: true,
+    },
+  })
+
+  const firstGameId = Number(mostPopular?.steam_id)
+  const secondGameId = Number(onTheRise?.steam_id)
+  const thirdGameId = Number(mostAnticipated?.steam_id)
 
   return (
     <div className="max-w-[800px] min-w-full md:min-w-[700px] flex grid-cols-2 grid-rows-2 flex-col gap-x-4 gap-y-6 mt-14 md:mt-36 md:grid">
@@ -24,7 +46,7 @@ export default async function CardGrid() {
         className="md:col-span-2"
         imageClassName="-mt-10 min-h-[180px]"
       />
-      {[secondGameId, TEKKEN_8].map((steamId) => (
+      {[secondGameId, thirdGameId].map((steamId) => (
         <GameCard key={steamId} steamId={steamId} />
       ))}
     </div>

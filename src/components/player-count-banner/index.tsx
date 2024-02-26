14ee8e4197523f360506255316d9cf584501394c
Joshua Kalejaiye <joshua.kalejaiye@sainsburys.co.uk>
2024-02-26 12:36:46 +0000

    chore: adjust size of player count banner

--- src/components/player-count-banner/index.tsx
+++ src/components/player-count-banner/index.tsx
@@ -5,7 +5,7 @@ export default async function PlayerCountBanner() {
 
   return (
     <div>
-      <h1 className="text-[5.2rem] font-extrabold tracking-tight text-lime-400 md:text-[10rem]">
+      <h1 className="text-[7rem] md:text-[12rem] font-extrabold tracking-tight text-lime-400 ">
         {totalPlayerCount?.toLocaleString(undefined)}
       </h1>
       <h2 className="-mt-6 pr-2 text-right text-xl font-semibold md:-mt-12">

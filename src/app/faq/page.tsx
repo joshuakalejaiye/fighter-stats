061f1a247b0cac4970ff414c440ee9daf520347b
Joshua Kalejaiye <joshua.kalejaiye@sainsburys.co.uk>
2024-02-26 14:19:53 +0000

    feat: added faq page

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,32 @@
+import ContentSection from '@/components/content-section'
+import Questions from '@/components/questions.tsx'
+
+export default async function Faq() {
+  const questions = [
+    {
+      question: `"Aren't these numbers lower than they should be ?"`,
+      answer:
+        'This only takes PC data into account, if Xbox and PlayStation ever decide to play nice and share their player numbers, I’ll add them to this.',
+    },
+    {
+      question: `"Why isn't my favourite game here"`,
+      answer:
+        'I’m sorry. It either isn’t on Steam or I had to prioritise other games, there is a limit on how many I can keep track of right now.',
+    },
+    {
+      question: 'Why is this data slightly different from Steam Charts ?',
+      answer:
+        'I honestly don’t know, I show you what Valve showed me in the last hour.',
+    },
+  ]
+
+  return (
+    <ContentSection className="">
+      <h1 className="text-5xl font-semibold pl-4 pb-4 text-left">FAQ</h1>
+
+      <div className="p-4 pb-8">
+        <Questions questions={questions} />
+      </div>
+    </ContentSection>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,10 @@ export const revalidate = 0
 
 export default async function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center text-white dark:bg-black">
-      <div className="container mt-6 flex flex-col items-center justify-center gap-12 px-4 py-16 min-w-full">
-        <PlayerCountBanner />
-        <CardGrid />
-        <GamesTable />
-        <footer>
-          <a
-            className="text-neutral-200 text-sm underline"
-            href={'https://www.joshuakal.dev'}
-            target="_blank"
-          >
-            made by joshuakalejaiye
-          </a>
-        </footer>
-      </div>
-    </main>
+    <>
+      <PlayerCountBanner />
+      <CardGrid />
+      <GamesTable />
+    </>
   )
 }
